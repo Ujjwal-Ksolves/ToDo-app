16c911fe99c2bf47ba04a96861012680cae514c5
root <root@ip-172-31-32-198.ap-south-1.compute.internal>
2023-10-03 08:44:36 +0000

    kube

--- client/src/pages/AddEdit.js
+++ client/src/pages/AddEdit.js
@@ -24,7 +24,7 @@ const handleSubmit = (e) => {
 	if(!task || !ls_date){
 	toast.error("Please provide value into each input field");
 	}else{
-	axios.post(`http://3.110.45.96:5000/api/post`,{
+	axios.post(`http://43.205.207.167:5000/api/post`,{
 	task,
 	ls_date
 	}).then(() => {
