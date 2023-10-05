e789935b78ff928b66c976cb23e5fb982cc5dea0
Ujjwal-Ksolves <144655871+Ujjwal-Ksolves@users.noreply.github.com>
2023-10-05 11:05:42 +0530

    Update AddEdit.js

--- client/src/pages/AddEdit.js
+++ client/src/pages/AddEdit.js
@@ -24,7 +24,7 @@ const handleSubmit = (e) => {
 	if(!task || !ls_date){
 	toast.error("Please provide value into each input field");
 	}else{
-	axios.post(`http://172.31.45.113:32500/api/post`,{
+	axios.post(`http://43.205.199.148:32500/api/post`,{
 	task,
 	ls_date
 	}).then(() => {
